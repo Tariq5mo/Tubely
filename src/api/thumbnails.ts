37440d76e087367acfe6f5a512b3eae3726b1718
Tariq5mo <tariqomer756@gmail.com>
2026-09-14 21:30:00 +0200

    Implement thumbnail upload functionality with size validation and user permissions

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,9 +1,9 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
 
 type Thumbnail = {
   data: ArrayBuffer;
@@ -11,6 +11,8 @@ type Thumbnail = {
 };
 
 const videoThumbnails: Map<string, Thumbnail> = new Map();
+
+const MAX_UPLOAD_SIZE = 10 << 20;
 
 export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -47,7 +49,37 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
 
   console.log("uploading thumbnail for video", videoId, "by user", userID);
 
-  // TODO: implement the upload here
+  const formData = req.formData();
+  const imageData = (await formData).get("thumbnail");
+
+  if (!(imageData instanceof File)) throw new BadRequestError("It's not file");
+
+  if (imageData.size > MAX_UPLOAD_SIZE)
+    throw new BadRequestError("More than 10MB");
+
+  const imageBytes = await imageData.arrayBuffer();
+
+  const video = getVideo(cfg.db, videoId);
+
+  if (!video) throw new NotFoundError("Not found");
+
+  if (video.userID !== userID) throw new UserForbiddenError("Not allowed");
+
+  const key: string = video.id;
+  const thumbnail: Thumbnail = {
+    data: imageBytes,
+    mediaType: imageData.type,
+  };
+
+  videoThumbnails.set(key, thumbnail);
+
+  const thumbnailURL = `http://localhost:${cfg.port}/api/thumbnails/${key}`;
+
+  video.thumbnailURL = thumbnailURL;
+
+  updateVideo(cfg.db, video);
+
+  respondWithJSON(200, video);
 
   return respondWithJSON(200, null);
 }
