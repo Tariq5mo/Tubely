6d4dee6317df8ca0ae7eeb0d5fb5372a1c64beee
Tariq5mo <tariqomer756@gmail.com>
2026-09-16 10:21:12 +0200

    Add missing import for S3Client and include it in ApiConfig

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,8 @@
+import { s3, S3Client } from "bun";
 import { newDatabase } from "./db/db";
 import type { Database } from "bun:sqlite";
+
+s3
 
 export type ApiConfig = {
   db: Database;
@@ -11,6 +14,7 @@ export type ApiConfig = {
   s3Region: string;
   s3CfDistribution: string;
   port: string;
+  s3Client: S3Client,
 };
 
 const pathToDB = envOrThrow("DB_PATH");
@@ -35,6 +39,7 @@ export const cfg: ApiConfig = {
   s3Region: s3Region,
   s3CfDistribution: s3CfDistribution,
   port: port,
+  s3Client: s3,
 };
 
 function envOrThrow(key: string) {
