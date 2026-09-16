b0d353b236f63b20c536406a93eed4b3259a1108
Tariq5mo <tariqomer756@gmail.com>
2026-09-16 09:12:37 +0200

    Use random file names for uploaded thumbnails to avoid collisions

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -5,6 +5,7 @@ import type { ApiConfig } from "../config";
 import { file, type BunRequest } from "bun";
 import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
 import path from "node:path";
+import { randomBytes } from "node:crypto";
 
 type Thumbnail = {
   data: ArrayBuffer;
@@ -36,9 +37,11 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
 
   if (imageType !== "image/jpeg" && imageType !== "image/png")
     throw new BadRequestError("The file must be image/jpeg or image/png");
-  
+
   const extension = imageType.split("/")[1];
   const imageBytes = await imageFile.arrayBuffer();
+
+  const nameFile = randomBytes(32).toString("base64");
 
   const video = getVideo(cfg.db, videoId);
 
@@ -48,12 +51,12 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
 
   const key: string = video.id;
 
-  const filePath = path.join(cfg.assetsRoot, `${videoId}.${extension}`);
+  const filePath = path.join(cfg.assetsRoot, `${nameFile}.${extension}`);
 
   const f = await Bun.write(filePath, imageBytes);
   if (!f) throw new Error("Can't create the file");
 
-  const thumbnailURL = `http://localhost:${cfg.port}/assets/${videoId}.${extension}`;
+  const thumbnailURL = `http://localhost:${cfg.port}/assets/${nameFile}.${extension}`;
 
   video.thumbnailURL = thumbnailURL;
 
